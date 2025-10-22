c6f7dae8ebb09b3745716b131253e26942d18745
LifeClear <lifeclearperu@gmail.com>
2025-10-22 13:23:56 -0500

    Subiendo archivos compilados
    Subiendo archivos compilados

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// [https://vitejs.dev/config/](https://vitejs.dev/config/)
-export default defineConfig({
-  plugins: [react()],
-  // Al ser un repositorio de usuario (lifeclear.github.io), la base es la raíz.
-  base: '/' 
-})
